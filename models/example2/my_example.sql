bb93619103f9d017bd1c06c2d2258473afaaa3d5
Ardhendu Singh <ardhendu.singh.s@gmail.com>
2024-02-17 03:18:10 +0000

    more test added

--- models/example2/my_example.sql
+++ models/example2/my_example.sql
@@ -1,1 +1,8 @@
-select * from `dbt-tutorial.jaffle_shop.customers`
+-- select * from `dbt-tutorial.jaffle_shop.customers`
+
+-- select *  from `dbt-tutorial`.jaffle_shop.customers
+
+-- SELECT * FROM `my-dbt-project-33004.dbt_asingh.customers` LIMIT 1000
+
+select * from `dbt-tutorial`.jaffle_shop.customers 
+where last_name in ('WX.')
